ea18601212b6f443d5617910319891f598fc2a74
fhtuthjhk <jsjcacksprocter@hotmail.com>
2024-05-05 13:21:20 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const flatten = (arr, depth = 1) =>
-  arr.reduce(
-    (a, v) =>
-      a.concat(depth > 1 && Array.isArray(v) ? flatten(v, depth - 1) : v),
-    [],
-  );
+console.log("hi world");
