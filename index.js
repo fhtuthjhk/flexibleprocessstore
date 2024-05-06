3036c67847222d78a4402bb66f72700c7b1831c7
fhtuthjhk <jsjcacksprocter@hotmail.com>
2024-05-06 15:34:35 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hi world");
+function isSymmetric(root) {
+  if (!root) return true;
+  return isMirror(root.left, root.right);
+  function isMirror(left, right) {
+    if (!left && !right) return true;
+    if (!left || !right || left.val !== right.val) return false;
+    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+  }
+}
